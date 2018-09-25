5ac6a3b5e4938bd3d8a9c3359f83e62377609eab
zhangshuang <397399964@qq.com>
2018-09-25 20:47:07 +0800

    my new project

---
+++ user_app/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+    return render(request, "index.html")
+
+#  处理登陆请求
+def login_action(request):
+    if request.method == "GET":
+        username = request.GET.get("username")
+        password = request.GET.get("password")
+
+        if username == "" or password == "":
+            return render(request, "index.html", {"error": "用户名或者密码错误"})
