fa711bf45833a6cb8f599f978f224f797ef33a78
zhangshuang <397399964@qq.com>
2018-09-26 23:36:37 +0800

    登陆

--- user_app/views.py
+++ user_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import auth
 
 # Create your views here.
 
@@ -8,9 +9,23 @@ def index(request):
 
 #  处理登陆请求
 def login_action(request):
-    if request.method == "GET":
-        username = request.GET.get("username")
-        password = request.GET.get("password")
+    if request.method == "POST":
+        username = request.POST.get("username", "")
+        password = request.POST.get("password", "")
 
         if username == "" or password == "":
-            return render(request, "index.html", {"error": "用户名或者密码错误"})
+            return render(request, "index.html", {"error": "用户名或者密码为空"})
+
+        else:
+            user = auth.authenticate(username = username, password = password)
+            if user is not None:
+                auth.login(request, user)
+                return render(request, "project_manage.html")
+            else:
+                return render(request, "index.html", {"error": "用户名或者密码错误"})
+
+        #if username == "admin" and password == "admin":
+           # return render(request, "project_manage.html")
+        #else:
+            #return render(request, "index.html", {"error": "用户名或者密码错误"})
+
