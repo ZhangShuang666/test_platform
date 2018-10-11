7e3a617e5a357e9b2826b80184161def84eb857f
zhangshuang666 <397399964@qq.com>
2018-10-11 18:19:51 +0800

    映射项目和模块表到admin管理后台

--- user_app/admin.py
+++ user_app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from user_app.models import Project, Module
+
 
 # Register your models here.
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'status', 'create_time', 'id']
+
+
+class ModuleAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'create_time', 'project', 'id']
+
+
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Module, ModuleAdmin)
